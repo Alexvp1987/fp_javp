c30ef0392a4790b148b471f160b9fdeeb8a92a68
Alexvp1987 <i2422060@continental.edu.pe>
2024-11-02 19:42:23 -0500

    validando codigo

--- semana02.py
+++ semana02.py
@@ -11,7 +11,7 @@ st.header("Ejemplo 1: Enteros ")
 st.write("En python, un entero (integer) es un número sin decimales por ejemplo:")
 
 #Input para que el usuario ingrese un dato (de preferencia un numero entero)
-int_variable = st.number_input("Ingrese un número entero", value = 42, step=1)
+int_variable = st.number_input("Ingrese un número entero:", value=42, step=1)
 
 #Mostrando el valor
 st.code(f"int_variable = {int_variable} # Tipo: {type(int_variable)}")
