926fa996ccc2c315fee677bf46b2be92007c3914
Alexvp1987 <i2422060@continental.edu.pe>
2024-11-02 19:26:44 -0500

    agregando descripcion

--- semana02.py
+++ semana02.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 #Titulo a la aplicacion
 st.title("Introducción a variables y tipos de datos en Python")
+
+#Descripcion inicial
+st.write("Python es un lenguaje de programacion dinamico donde ... ") 
