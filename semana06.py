8e0aa666b873de1ec422241ae174e71d230540f5
Alexvp1987 <i2422060@continental.edu.pe>
2024-11-02 21:41:29 -0500

    agregando archivo semana 6

---
+++ semana06.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#Ejercicio 5: Generar e imprimir los números pares entre 0 y 100
+st.title("Ejercicios de números pares")
+
+st.subheader("Ejercicio 5: Números pares entre 0 y 100 ")
+pares_0_100 = [i for i range(0,101) if i % 2 ==0]
+st.write("Numeros pares entre 0 y 100:")
+st.write(pares_0_100)
