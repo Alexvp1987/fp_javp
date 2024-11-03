bb1e3fdbe761b917cbd111ced6d6ebb901737138
Alexvp1987 <i2422060@continental.edu.pe>
2024-11-02 21:47:36 -0500

    agregando in en for

--- semana06.py
+++ semana06.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("Ejercicios de números pares")
 
 st.subheader("Ejercicio 5: Números pares entre 0 y 100 ")
-pares_0_100 = [i for i range(0,101) if i % 2 ==0]
+pares_0_100 = [i for i in range(0,101) if i % 2 ==0]
 st.write("Numeros pares entre 0 y 100:")
 st.write(pares_0_100)
